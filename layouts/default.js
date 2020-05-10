5c51468f26a43b11f60132725464c07a1741f916
Ilia Trifonov <eley81@gmail.com>
2020-05-10 18:05:40 +0300

    Update default layout

---
+++ layouts/default.js
@@ -0,0 +1,9 @@
+import PageHeader from '@/components/sections/PageHeader/PageHeader.vue'
+import PageFooter from '@/components/sections/PageFooter/PageFooter.vue'
+
+export default {
+  components: {
+    PageHeader,
+    PageFooter
+  }
+}
