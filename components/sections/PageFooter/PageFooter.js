fe18b2ae929472742177c0a7e761ecf289290379
Ilia Trifonov <eley81@gmail.com>
2020-05-10 18:07:46 +0300

    Add PageFooter component

---
+++ components/sections/PageFooter/PageFooter.js
@@ -0,0 +1,9 @@
+import DevelopedBy from '@/components/blocks/DevelopedBy/DevelopedBy.vue'
+import Logo from '@/components/blocks/Logo/Logo.vue'
+
+export default {
+  components: {
+    DevelopedBy,
+    Logo
+  }
+}
---
+++ components/sections/PageFooter/PageFooter.vue
@@ -0,0 +1,3 @@
+<template src="./PageFooter.pug" lang="pug"></template>
+<script src="./PageFooter.js"></script>
+<style src="./PageFooter.scss" lang="scss"></style>
