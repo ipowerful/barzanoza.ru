e9e3fad41c546709b00c60573b7482343db3a57f
Ilia Trifonov <eley81@gmail.com>
2020-05-10 18:07:08 +0300

    Add logo component

---
+++ components/blocks/Logo/Logo.js
@@ -0,0 +1,3 @@
+import '@/assets/symbols/zanoza-logo.svg'
+
+export default {}
---
+++ components/blocks/Logo/Logo.vue
@@ -0,0 +1,3 @@
+<template src="./Logo.pug" lang="pug"></template>
+<script src="./Logo.js"></script>
+<style src="./Logo.scss" lang="scss"></style>
