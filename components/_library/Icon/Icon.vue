e19852433581782d34da4ef97a76508d9d32a5bb
Ilia Trifonov <eley81@gmail.com>
2020-07-18 14:59:06 +0300

    Add Icon component

---
+++ components/_library/Icon/Icon.vue
@@ -0,0 +1,13 @@
+<style lang="scss">
+  .icon {
+    position: relative;
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
+  .icon__svg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+</style>
