f4ca95a1f89f8ca08d3561aa0e78b61b38b1a053
Ilia Trifonov <eley81@gmail.com>
2020-07-18 14:59:20 +0300

    Add Footer component

--- components/sections/PageFooter/PageFooter.js
+++ components/sections/PageFooter/PageFooter.js
@@ -1,5 +1,5 @@
-import DevelopedBy from '@/components/blocks/DevelopedBy/DevelopedBy.vue'
-import Logo from '@/components/blocks/Logo/Logo.vue'
+import DevelopedBy from '~/components/blocks/DevelopedBy/DevelopedBy.vue'
+import Logo from '~/components/_library/Logo/Logo.vue'
 
 export default {
   components: {
