f22a30756b86fade2944ca9ce1db7ab87cd9ce27
Ilia Trifonov <eley81@gmail.com>
2020-07-18 14:59:35 +0300

    Add Header component

--- components/sections/PageHeader/PageHeader.js
+++ components/sections/PageHeader/PageHeader.js
@@ -1,3 +1,11 @@
-export default {
+import Logo from '~/components/_library/Logo/Logo.vue'
+import Navbar from '@/components/blocks/Navbar/Navbar.vue'
+import Cart from '@/components/blocks/Cart/Cart.vue'
 
+export default {
+  components: {
+    Logo,
+    Navbar,
+    Cart
+  }
 }
