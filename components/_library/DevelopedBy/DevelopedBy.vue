1f7cb36f7ffb7301b169206056079b9c2a334cc3
Ilia Trifonov <eley81@gmail.com>
2020-07-21 06:54:27 +0300

    Update developed by logo

---
+++ components/_library/DevelopedBy/DevelopedBy.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="redgarnet">
+    <span class="redgarnet__title">Создание сайта</span>
+    <a class="redgarnet__link" :href="url" title="iPowerful" target="_blank">iPowerful</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    url: 'https://ipowerful.design'
+  }),
+  mounted () {
+    console.log('\n %c Magic is created by iPowerful %c ' + this.url + '\n', 'color: #fff; background: #00cccc; padding:5px;', 'background: #fff;')
+  }
+}
+</script>
+
+<style src="./DevelopedBy.scss" lang="scss"></style>
