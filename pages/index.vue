326b01d36e324bb4c6230d5b3493b6e0a8926101
Ilia Trifonov <eley81@gmail.com>
2020-07-22 17:49:42 +0300

    Update default page component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
-<template src="./index.pug" lang="pug"></template>
-<script src="./index.js"></script>
-<style src="./index.scss" lang="scss"></style>
+<template>
+  <slider />
+</template>
+
+<script>
+import Slider from '~/components/_library/Slider/Slider.vue'
+
+export default {
+  components: {
+    Slider
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
