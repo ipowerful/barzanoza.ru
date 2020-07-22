33bf48bcf89294d93f56a761335b2a41532a7e2d
Ilia Trifonov <eley81@gmail.com>
2020-07-22 17:51:09 +0300

    Create slider component alpha

---
+++ components/_library/Slider/Slider.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-swiper:mySwiper="swiperOption">
+    <div class="swiper-wrapper">
+      <div v-for="banner in banners" :key="banner" class="swiper-slide">
+        <img :src="banner">
+      </div>
+    </div>
+    <div class="swiper-pagination" />
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+// import 'swiper/css/swiper.css'
+import 'swiper/swiper-bundle.css'
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  },
+  data () {
+    return {
+      banners: [
+        '/zanoza-promo1.jpg',
+        '/zanoza-promo2.jpg',
+        '/zanoza-promo3.jpg'
+      ],
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      }
+    }
+  },
+  mounted () {
+    console.log('Current Swiper instance object', this.mySwiper)
+    this.mySwiper.slideTo(3, 1000, false)
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
