d4866f9fc5a65b15264ef57e07b29006486871a0
Ilia Trifonov <eley81@gmail.com>
2020-07-24 07:40:34 +0300

    Update cart component

--- components/_library/Cart/Cart.vue
+++ components/_library/Cart/Cart.vue
@@ -1,3 +1,33 @@
-<template src="./Cart.pug" lang="pug"></template>
-<script src="./Cart.js"></script>
-<style src="./Cart.scss" lang="scss"></style>
+<template>
+  <div class="cart">
+    <a href="#" class="cart__link">
+      <icon :name="`shopping-bag`" class="cart__icon" />
+    </a>
+  </div>
+</template>
+
+<script>
+import Icon from '~/components/_library/Icon/Icon.vue';
+
+export default {
+  components: {
+    Icon
+  }
+}
+</script>
+
+<style lang="scss">
+.cart {
+  &__icon {
+    width: 24px;
+    height: 24px;
+  }
+  /* 
+  &__link {
+    
+    &:hover {
+      color: $primary;
+    }
+  } */
+}
+</style>
